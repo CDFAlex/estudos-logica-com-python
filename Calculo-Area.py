86c5408ed0df0db839bb303ccb33b7bd5946f758
CDFAlex <alexfermino1@gmail.com>
2025-06-22 18:31:38 -0300

    Update Calculo-Area.py
    Melhorias na função de calculo de area

--- Calculo-Area.py
+++ Calculo-Area.py
@@ -1,10 +1,34 @@
-import math
+import calcArea
 
-def calcular_area_circulo(raio):
-    return math.pi * raio**2
+def main():
+    print("Cálculo das áreas de figuras geométricas:")
+    print("1. Círculo")
+    print("2. Triângulo")
+    print("3. Retângulo")
 
-def calcular_area_triangulo(base, altura):
-    return (base * altura) / 2
+    try:
+        opcao = int(input("Qual figura deseja calcular a área? "))
 
-def calcular_area_retangulo(largura, altura):
-    return largura * altura
+        if opcao < 1 or opcao > 3:
+            print("ERRO! Essa não é uma opção válida!")
+        else:
+            if opcao == 1:
+                raio = float(input("Digite o raio do círculo: "))
+                area = calcArea.calcular_area_circulo(raio)
+                print(f"A área do círculo é: {area:.2f}")
+            elif opcao == 2:
+                base = float(input("Digite a base do triângulo: "))
+                altura = float(input("Digite a altura do triângulo: "))
+                area = calcArea.calcular_area_triangulo(base, altura)
+                print(f"A área do triângulo é: {area:.2f}")
+            elif opcao == 3:
+                largura = float(input("Digite a largura do retângulo: "))
+                altura = float(input("Digite a altura do retângulo: "))
+                area = calcArea.calcular_area_retangulo(largura, altura)
+                print(f"A área do retângulo é: {area:.2f}")
+    except ValueError:
+        print("ERRO! Essa não é uma opção válida!")
+
+
+if __name__ == "__main__":
+    main()
